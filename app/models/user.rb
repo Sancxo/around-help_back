c020e34989a01482f0635b4b807a73a9e8fd7fb6
Simon <simon@7lieues.io>
2022-09-06 16:24:06 +0200

    fix(registration): makes Address references optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist 
 
-  belongs_to :address
+  belongs_to :address, optional: true
   has_many :user_need
 
   has_one_attached :id_card
