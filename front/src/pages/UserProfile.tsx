c43b0bc09e2f746e82c356ffa3445a42c6663429
Simon <simon@7lieues.io>
2022-09-17 21:38:18 +0200

    fix(profile): fixes birthdate display

--- front/src/pages/UserProfile.tsx
+++ front/src/pages/UserProfile.tsx
@@ -8,8 +8,7 @@ export default function UserProfile({ user }: { user: User }): ReactElement {
             <div>
                 <p>Hello {user.first_name} {user.last_name}!</p>
                 <p>{user.email}</p>
-                {/* For an unknown reason, I have to use a fragment (<></>) to display this variable ... */}
-                <p><>You are born: {user.birthdate}</></p>
+                <p>You are born: {user.birthdate?.getDate()}/{user.birthdate?.getMonth()}/{user.birthdate?.getFullYear()}</p>
                 <p>About you: {user.about}</p>
 
             </div >
