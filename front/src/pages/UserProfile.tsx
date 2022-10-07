3d124359cad6dd42c9532cf16f383c7ab8896fc5
Simon <simon@7lieues.io>
2022-10-07 12:23:44 +0200

    feat(user_profile): call to new /users/:id route

--- front/src/pages/UserProfile.tsx
+++ front/src/pages/UserProfile.tsx
@@ -1,20 +1,35 @@
-import { ReactElement } from "react";
+import axios from "axios";
+import { ReactElement, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import User from "../shared/interfaces/user.interface";
 
-export default function UserProfile({ user }: { user: User }): ReactElement {
-    /* we also have to check if user.id === id in the url or not (useParams ?) */
-    if (user.id) {
-        return (
-            <div>
-                <p>Hello {user.first_name} {user.last_name}!</p>
-                <p>{user.email}</p>
-                <p>You are born: <>{user.birthdate}</></p>
-                <p>About you: {user.about}</p>
+export default function UserProfile({ defaultUser }: { defaultUser: User }): ReactElement {
+    const urlParams = useParams();
+    console.log(urlParams.id);
 
-            </div >
-        )
-    } else {
+    const [userProfile, setUserProfile] = useState<User>(defaultUser)
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_BACKEND_URL}/users/${urlParams.id}`)
+            .then(resp => {
+                console.log("We get User: ", resp.data.user);
+                setUserProfile(resp.data.user);
+                setIsLoaded(true);
+            })
+            .catch(err => {
+                console.log(err);
+                setIsLoaded(true);
+                setError(true)
+            })
+    }, [urlParams.id])
+
+    if (!isLoaded) return <div><p>Please, wait ...</p></div>
+
+    if (error || !userProfile) {
         return (
             <div>
                 <p>Oops ... This user doesn't seem to exists !</p>
@@ -22,4 +37,12 @@ export default function UserProfile({ user }: { user: User }): ReactElement {
         )
     }
 
+    return (
+        <div>
+            <p>Hello {userProfile.first_name} {userProfile.last_name}!</p>
+            <p>{userProfile.email}</p>
+            <p>You are born: <>{userProfile.birthdate}</></p>
+            {userProfile.about && <p>About you: {userProfile.about}</p>}
+        </div >
+    )
 }   
