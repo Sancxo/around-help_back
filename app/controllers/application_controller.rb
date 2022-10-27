a8efcd6002bd838a331f151d288ec6cc56b5d1fb
Simon <simon@7lieues.io>
2022-10-27 22:22:03 +0200

    fix(user): adds avatar to permitted fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,13 @@ class ApplicationController < ActionController::API
 
     def config_permitted_params
         devise_parameter_sanitizer.permit(:sign_up) do |u|
-            u.permit(:first_name, :last_name, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :address_id)
+            u.permit(:first_name, :last_name, :avatar, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :address_id)
         end
         devise_parameter_sanitizer.permit(:sign_in) do |u|
             u.permit(:email, :password)
         end
         devise_parameter_sanitizer.permit(:account_update) do |u|
-            u.permit(:first_name, :last_name, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :current_password, :address_id) 
+            u.permit(:first_name, :last_name, :avatar, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :current_password, :address_id) 
         end
     end
 end
