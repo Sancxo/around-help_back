a6e6f94eefda3b844afa5aa18b36304514d39f27
Simon <simon@7lieues.io>
2022-10-27 21:43:49 +0200

    feat(user): allows .pdf format for id_card validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   has_one_attached :id_card
   has_one_attached :avatar
 
-  validates :id_card, attached: true, content_type: {in: ['image/png', 'image/jpeg', 'image/gif', 'image/webp', 'image/avif'], message: "Not a supported image format. Should be either .png, .jpeg, .gif, .webp or .avif"}
+  validates :id_card, attached: true, content_type: {in: ['application/pdf', 'image/png', 'image/jpeg', 'image/gif', 'image/webp', 'image/avif'], message: "Not a supported image format. Should be either .pdf, .png, .jpeg, .gif, .webp or .avif"}
   validates :avatar, attached: true, content_type: {in: ['image/png', 'image/jpeg', 'image/gif', 'image/webp', 'image/avif'], message: "Not a supported image format. Should be either .png, .jpeg, .gif, .webp or .avif"}
 end
