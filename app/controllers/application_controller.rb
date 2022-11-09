5d5b5ea27a3296db4c8e70610c70f1b7245b9418
Simon <simon@7lieues.io>
2022-11-09 22:50:00 +0100

    refactor(user): minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,11 @@ class ApplicationController < ActionController::API
     protected
 
     def config_permitted_params
-        devise_parameter_sanitizer.permit(:sign_up) do |u|
-            u.permit(:first_name, :last_name, :avatar, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :address_id)
-        end
-        devise_parameter_sanitizer.permit(:sign_in) do |u|
-            u.permit(:email, :password)
-        end
-        devise_parameter_sanitizer.permit(:account_update) do |u|
-            u.permit(:first_name, :last_name, :avatar, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :current_password, :address_id) 
-        end
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :address_id])
+
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar, :id_card, :birthdate, :about, :email, :password, :password_confirmation, :current_password, :address_id])
     end
+
 end
