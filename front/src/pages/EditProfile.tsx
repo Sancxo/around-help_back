8e568e2029f4bccf0b29bc8a33a938e44bfe7ff0
Simon <simon@7lieues.io>
2022-11-25 12:31:37 +0100

    feat(edit profile): adds an avatar preview when editing it

--- front/src/pages/EditProfile.tsx
+++ front/src/pages/EditProfile.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TokenContext, UserContext } from "../shared/context";
-import { update } from "../shared/helpers/user.helper";
+import { defaultUser, update } from "../shared/helpers/user.helper";
 
 import { RegistrationValues } from "../shared/interfaces/user.interfaces";
 
@@ -17,17 +17,13 @@ export default function EditProfile(): ReactElement {
   });
   const { user, setUser } = useContext(UserContext);
   const { token, setToken } = useContext(TokenContext);
+
+  const [avatar, setAvatar] = useState("");
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(false);
 
 
   const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (token) {
-  //     getUserInfos(`${user.id!}`, token, setUserProfile, setIsLoaded, setError);
-  //   }
-  // }, [token, user.id])
 
   function handleInputs(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     const name = e.target.name;
@@ -36,13 +32,16 @@ export default function EditProfile(): ReactElement {
     setRegistrationValues(registrationValues => ({ ...registrationValues, [name]: value }));
   }
   function handleImages(e: React.ChangeEvent<HTMLInputElement>) {
-    (e.target.id === "avatar-input") && setRegistrationValues(registrationValues => ({ ...registrationValues, 'avatar': e.target.files![0] }));
-    e.target.id === "id-card-input" && setRegistrationValues(registrationValues => ({ ...registrationValues, 'id_card': e.target.files![0] }));
+    const imgFile = e.target.files![0];
+    e.target.id === "avatar-input" && setRegistrationValues(registrationValues => ({ ...registrationValues, 'avatar': imgFile }));
+    setAvatar(URL.createObjectURL(imgFile));
   }
 
   function handleSubmit() {
     update(registrationValues, setUser, setToken, navigate);
   }
+
+  console.log(user)
 
   return (
     <form name="user" id="user-infos" encType='multipart/form-data' className="container">
@@ -56,6 +55,8 @@ export default function EditProfile(): ReactElement {
         <label htmlFor="last-name-input">Last name:</label>
         <input type="text" name="last_name" id="last-name-input" value={user.last_name as string} onChange={handleInputs} />
 
+        <img src={`${avatar ? avatar : (user.avatar ? user.avatar : defaultUser.avatar)}`} alt="avatar" width={200} height={200} />
+
         <label htmlFor="avatar-input">Profile picture:</label>
         <input type="file" name="avatar" id="avatar-input" accept="image/png, image/jpeg, image/gif, image/webp, image/avif" onChange={handleImages} />
 
@@ -67,9 +68,6 @@ export default function EditProfile(): ReactElement {
 
         <label htmlFor="email-input">Email:</label>
         <input type="email" name="email" id="email-input" value={user.email as string} onChange={handleInputs} />
-
-        <label htmlFor="id-card-input">Id card:</label>
-        <input type="file" name="id_card" id="id-card-input" accept="application/pdf, image/png, image/jpeg, image/gif, image/webp, image/avif" onChange={handleImages} />
 
         <input type="button" className="btn-prim" value="Submit" onClick={handleSubmit} />
       </fieldset>
