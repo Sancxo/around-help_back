40fc0ac27d3c19278c7246471e48b6b3c7c32dae
Simon <simon.tirant@gmail.com>
2022-12-04 00:08:58 +0100

    refactor(Address): removes geocoder code for long_lat

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,15 +2,15 @@ class Address < ApplicationRecord
     has_many :users
     has_many :needs
 
-    geocoded_by :stringified_address, if: ->(obj){ obj.address.present? and obj.address_changed? } do |obj, res|
-        if geo = res.first
-            obj.long_lat = [geo.longitude, geo.latitude]
-        end
-    end
-    after_validation :geocode
-    # , :if => :address_changed?
+    # geocoded_by :stringified_address, if: ->(obj){ obj.address.present? and obj.address_changed? } do |obj, res|
+    #     if geo = res.first
+    #         obj.long_lat = [geo.longitude, geo.latitude]
+    #     end
+    # end
+    # after_validation :geocode
+    # # , :if => :address_changed?
 
-    def stringified_address
-        [number, street, city, country].compact.join(', ')
-    end
+    # def stringified_address
+    #     [number, street, city, country].compact.join(', ')
+    # end
 end
