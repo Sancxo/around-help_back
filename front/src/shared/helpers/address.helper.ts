d24a2a3b28fa9a756fe3996d33d0f67633b2bf9f
Simon <simon.tirant@gmail.com>
2023-01-03 15:34:36 +0100

    refactor(address): changes type for address data and adds context function to store address in state

--- front/src/shared/helpers/address.helper.ts
+++ front/src/shared/helpers/address.helper.ts
@@ -1,19 +1,22 @@
 import axios, { AxiosResponse } from "axios";
 import { NavigateFunction } from "react-router-dom";
-import { Address, FlashMessage, setContext } from "../interfaces/misc.interfaces";
+import { Address, AddressValues, FlashMessage, setContext } from "../interfaces/misc.interfaces";
 import User from "../interfaces/user.interfaces";
 import { updateUser } from "./user.helper";
 
 async function registerAddress(
-  data: Address,
+  data: AddressValues,
   setUser: setContext<User>,
+  setAddress: setContext<Address>,
   setFlashMessage: setContext<FlashMessage>,
   navigate: NavigateFunction
 ): Promise<any> {
   return await axios
-    .post<Address, AxiosResponse<any, any>>(`${process.env.REACT_APP_BACKEND_URL}/addresses`, data)
+    .post<AddressValues, AxiosResponse<any, any>>(`${process.env.REACT_APP_BACKEND_URL}/addresses`, data)
     .then((resp): void => {
       if (resp.status === 201) {
+        setAddress(resp.data.address);
+
         updateUser({ address_id: resp.data.address.id }, setUser, setFlashMessage)
           .then(respUpdate => {
             navigate(`/user/${respUpdate.user.id}`)
