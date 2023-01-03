0ed719ba8de87ca86d69f08dba0b3be4ec185c27
Simon <simon.tirant@gmail.com>
2023-01-03 15:33:13 +0100

    refactor(address): modifies context for Address according to the new Address interface with an id

--- front/src/shared/context.tsx
+++ front/src/shared/context.tsx
@@ -5,7 +5,7 @@ import User from "./interfaces/user.interfaces";
 
 export default function AppProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User>(defaultUser);
-  const [address, setAddress] = useState<Address>({ address: "", long_lat: [0, 0] });
+  const [address, setAddress] = useState<Address>({ id: 0, address: "", lat_lng: { lat: 0, lng: 0 } });
   const [token, setToken] = useState("");
   const [flashMessage, setFlashMessage] = useState<FlashMessage>([Nil, ""]);
 
@@ -19,13 +19,12 @@ export default function AppProvider({ children }: { children: ReactNode }) {
         </AddressContext.Provider>
       </UserContext.Provider>
     </TokenContext.Provider>
-
   )
 }
 
 const UserContext = createContext({ user: defaultUser, setUser: (user: User): void => { } });
 
-const AddressContext = createContext({ address: { address: "", long_lat: [0, 0] }, setAddress: (address: Address): void => { } });
+const AddressContext = createContext({ address: { id: 0, address: "", lat_lng: { lat: 0, lng: 0 } }, setAddress: (address: Address): void => { } });
 
 const TokenContext = createContext({ token: "", setToken: (token: string): void => { } });
 
