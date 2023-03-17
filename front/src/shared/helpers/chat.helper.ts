8703880544548e5a4ed90e10ec3bcdb8ebfb4857
Simon <simon.tirant@gmail.com>
2023-03-17 22:51:45 +0100

    feat(ChatMessages): creates API functions to fetch messages and send a new message

--- front/src/shared/helpers/chat.helper.ts
+++ front/src/shared/helpers/chat.helper.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosResponse } from "axios";
+import { Dispatch, SetStateAction } from "react";
+import { ChatMessage } from "../interfaces/misc.interfaces";
 
 async function createChatRoom(needId: number): Promise<any> {
   axios
@@ -26,4 +28,17 @@ async function getChatRoomFromNeedId(needId: number): Promise<number> {
     .catch(err => console.error(err))
 }
 
-export { createChatRoom, createChatRoomUser, getChatRoomFromNeedId }
+async function getChatMessages(chat_room_id: string, setMessages: Dispatch<SetStateAction<ChatMessage[]>>) {
+  return await axios
+    .get<ChatMessage[]>(`${process.env.REACT_APP_BACKEND_URL}/chat_messages`, { withCredentials: true })
+    .then(resp => setMessages(resp.data))
+    .catch(err => console.error(err))
+}
+
+async function sendChatMessage(messageBody: string, userId: number, chatRoomId: string) {
+  return await axios
+    .post(`${process.env.REACT_APP_BACKEND_URL}/chat_messages`, { body: messageBody, user_id: userId, chat_room_id: chatRoomId }, { withCredentials: true })
+    .catch(err => console.error(err));
+}
+
+export { createChatRoom, createChatRoomUser, getChatRoomFromNeedId, getChatMessages, sendChatMessage }
