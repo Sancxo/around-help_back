0ecceec289790483a02affc4a400022b798db874
Simon <simon.tirant@gmail.com>
2023-03-22 23:27:21 +0100

    feat: verifies if verified_user is allowed to participate to the specified chat_room

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,18 +3,33 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      # Connection ...
+    end
+
+    def receive(data)
+      parsed_data = ActiveSupport::JSON.decode(data)
+
+      parsed_message = ActiveSupport::JSON.decode(parsed_data["identifier"])
+
+      chat_room_id = parsed_message["room_id"]
+
+      self.current_user = find_verified_user(chat_room_id)
+
+      send_async :dispatch_websocket_message, data
     end
 
     private
     
-    def find_verified_user
+    def find_verified_user(chat_room_id)
       jwt_payload = JWT.decode(
         cookies.signed[:jwt], 
         Rails.application.credentials.devise[:jwt_secret_key]
       ).first
 
-      if verified_user = User.find_by(id: jwt_payload['id']) 
+      verified_user = User.includes(:chat_rooms).find_by(id: jwt_payload['id'])
+      chat_room = ChatRoom.find(chat_room_id)
+
+      if verified_user && verified_user.chat_rooms.include?(chat_room)
         verified_user
       else 
         reject_unauthorized_connection
