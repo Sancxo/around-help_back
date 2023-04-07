a01520eaacc2ab3b08d473701145eedbd9414a88
Simon <simon.tirant@gmail.com>
2023-04-07 13:09:35 +0200

    refactor: adds pad leading 0 for days, months, hours, minutes and seconds with only one digit

--- front/src/shared/helpers/misc.helper.ts
+++ front/src/shared/helpers/misc.helper.ts
@@ -14,12 +14,26 @@ function clearFlash(setFlashMessage: setContext<FlashMessage>) {
 
 function readDate(date: Date | string) {
   date = typeof date === "string" ? new Date(date) : date;
-  return `${date.getFullYear()} ${date.getMonth()} ${date.getDate()}`
+
+  const year = String(date.getFullYear());
+  const month = String(date.getMonth()).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`
 }
 
 function readDateTime(datetime: Date | string) {
   datetime = typeof datetime === "string" ? new Date(datetime) : datetime;
-  return `${datetime.getFullYear()} ${datetime.getMonth()} ${datetime.getDate()}, at: ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`
+
+  const year = String(datetime.getFullYear());
+  const month = String(datetime.getMonth()).padStart(2, "0");
+  const day = String(datetime.getDate()).padStart(2, "0");
+
+  const hours = String(datetime.getHours()).padStart(2, "0");
+  const minutes = String(datetime.getMinutes()).padStart(2, "0");
+  const seconds = String(datetime.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day}, ${hours}:${minutes}:${seconds}`
 }
 
 export { clearFlash, getFlash, readDate, readDateTime }
