5099be93bfacd9b2162120d36afd7081d300c4f8
Simon <simon.tirant@gmail.com>
2023-04-12 21:40:25 +0200

    feat: adds conditionnal button to homepage to log in or go to Needs

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,9 +1,14 @@
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement, useContext, useEffect, useState } from "react";
 import { countUnfulfilledNeeds } from "../shared/helpers/needs.helper";
+import { UserContext } from "../shared/context";
+import User from "../shared/interfaces/user.interfaces";
+import { Link } from "react-router-dom";
 
 
 export default function Home(): ReactElement {
     const [unfulfilledNeedsCount, setUnfulfilledNeedsCount] = useState(0);
+
+    const user: User = useContext(UserContext).user;
 
     useEffect(() => {
         countUnfulfilledNeeds(setUnfulfilledNeedsCount);
@@ -19,6 +24,11 @@ export default function Home(): ReactElement {
             <div id="hero-text">
                 <h2>We Need You!</h2>
                 <h3>Number of unfulfilled requests: {unfulfilledNeedsCount}</h3>
+                {
+                    user.id === 0 ?
+                        <Link to={'/login'}><button type="button" className="btn-prim text-black">Log in</button></Link> :
+                        <Link to={'/needs'}><button type="button" className="btn-prim text-black">Answer to a Need</button></Link>
+                }
             </div>
         </div>
     );
