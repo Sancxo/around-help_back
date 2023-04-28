f0299cefa4b5059b0dfd78acfd301c01002d9ea1
Simon <simon.tirant@gmail.com>
2023-04-28 17:31:04 +0200

    fix: changes headers in cors params

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins ENV['FRONT_URL']
 
     resource "*",
-    headers: :any,
+    headers: 'x-domain-token',
     methods: [:get, :post, :put, :patch, :delete, :options, :head],
     expose: %w[Authorization Uid],
     credentials: true
